b8326868ea52c4ed1f3bbb897f31689134a6d1c5
ded <llilyakusa13@gmail.com>
2026-01-25 14:38:15 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ PADDLE_SPEED = 10
 COUNTDOWN_START = 3
 
 class GameServer:
-    def __init__(self, host='localhost', port=8080):
+    def __init__(self, host='localhost', port=8081):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen(2)
